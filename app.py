6e80e5d91641ad0ef9036373212dd1de89fa0ad3
Dzook1 <147112512+Dzook1@users.noreply.github.com>
2024-04-23 07:45:02 -0400

    Base Customer basic setup

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+
+# conn_str = "mysql://root:cset155@localhost/ecommerce"
+# engine = create_engine(conn_str, echo=True)
+# conn = engine.connect()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/baseCustomer.html')
+def baseCustomer():
+    return render_template('baseCustomer.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
