9d6a8003d5ab75e0c06f0731d2e830bc4c549db2
Dzook1 <147112512+Dzook1@users.noreply.github.com>
2024-04-24 16:50:52 -0400

    products page front end

--- app.py
+++ app.py
@@ -83,6 +83,10 @@ def loginEmpGo():
         return render_template('empLanding.html')
     else:
         return render_template('index.html')
+    
+@app.route('/products.html')
+def products():
+    return render_template('products.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
