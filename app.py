b3ed7bc726e246743827f49717dfa9bff6fad062
Florglsnorp <nicholaslink234@gmail.com>
2024-05-09 12:43:03 -0400

    Orders show

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, text
 import ctypes
 from datetime import date
 
-conn_str = "mysql://root:cset155@localhost/ecommerce"
+conn_str = "mysql://root:Dougnang1@localhost/ecommerce"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
